615d0c2d3352a995052adfed4d15b4e933923386
Jenil Padshala <jenilpadshala@gmail.com>
2025-02-09 14:31:13 +0530

    added yolov8_int8 model and corresponding label_text.txt

--- app/src/main/java/com/example/visual_assistance_system/Constants.kt
+++ app/src/main/java/com/example/visual_assistance_system/Constants.kt
@@ -1,6 +1,6 @@
 package com.example.visual_assistance_system
 
 object Constants {
-    const val MODEL_PATH = "yolov8n_int8.tflite"
-    val LABELS_PATH: String? = "example_label_file.txt" // provide your labels.txt file if the metadata not present in the model
+    const val MODEL_PATH = "best_int8.tflite"
+    val LABELS_PATH: String? = "label_text.txt" // provide your labels.txt file if the metadata not present in the model
 }
